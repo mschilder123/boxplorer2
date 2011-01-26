8bbf4525a22740ada2787827b3a8262ae61b8b37
marius.schilder@gmail.com <marius.schilder@gmail.com@29ca6bc3-2d16-8ea8-1f4c-e0f4d2eeda2c>
2011-01-26 04:58:43 +0000

    Better menger-sphere shader.

--- trunk/cfgs/menger-sphere.cfg.data/fragment.glsl
+++ trunk/cfgs/menger-sphere.cfg.data/fragment.glsl
@@ -1,5 +1,5 @@
-// Menger-sphere shader by marius.
-// http://www.fractalforums.com/3d-fractal-generation/revenge-of-the-half-eaten-menger-sponge/
+// Menger-sphere shader by marius. Parts by knighty, Rrrola.
+// See http://www.fractalforums.com/3d-fractal-generation/revenge-of-the-half-eaten-menger-sponge/
 
 #define MAX_DIST 4.0
 
@@ -21,13 +21,11 @@ uniform int iters;  // Number of fractal iterations. {min=1 max=100}
 uniform int color_iters;  // Number of fractal iterations for coloring. {min=1 max=100}
 uniform int max_steps;  // Maximum raymarching steps. {min=1 max=200}
 
-#define SIGN par[0].z  // {min=-1 max=1 step=2}
-
 // Colors. Can be negative or >1 for interesting effects.
 #define backgroundColor par[7]
 #define surfaceColor1 par[2]
-#define surfaceColor2 par[3]
-#define surfaceColor3 par[4]
+//#define surfaceColor2 par[3]
+//#define surfaceColor3 par[4]
 #define specularColor par[5]
 #define glowColor par[6]
 #define lightVector par[1]
@@ -36,6 +34,7 @@ uniform int max_steps;  // Maximum raymarching steps. {min=1 max=200}
 #define SCALE par[8].y  // {min=1 max=5 step=.1}
 #define INTRA_D par[8].z  // {min=.01 max=10.0 step=.01}
 #define INITIAL_K par[0].x  // {min=0 max=5 step=.1}
+#define DE_EPS par[0].y  // {min=.0000001 max=.001 step=.0000001}
 
 vec3 aoColor = vec3(0, 0, 0);
 
@@ -43,22 +42,21 @@ vec3 aoColor = vec3(0, 0, 0);
 #define d d_SphereSponge
 #define color color_SphereSponge
 
-// See http://www.fractalforums.com/3d-fractal-generation/revenge-of-the-half-eaten-menger-sponge/
-vec3 color_SphereSponge(vec3 pos) {
-  return surfaceColor1;
-}
-
 float d_SphereSponge(vec3 pos) {
   float k = INITIAL_K;
   float d = -100.0;
-  for(int i=0; i<color_iters && (d < .5); i++) {
+  for(int i=0; i<iters; i++) {
     vec3 x = mod(pos * k, MOD) - .5 * MOD;
-    float r = length(x.xyz);
+    float r = length(x);
     float d1 = (INTRA_D - r) / k;
     d = max(d, d1);
     k *= SCALE;
   }
   return d;
+}
+
+vec3 color_SphereSponge(vec3 pos) {
+  return surfaceColor1;
 }
 
 float normal_eps = 0.00001;
@@ -102,58 +100,53 @@ vec3 blinn_phong(vec3 normal, vec3 view, vec3 light, vec3 diffuseColor) {
 
 
 // Ambient occlusion approximation.
-float ambient_occlusion(vec3 p, vec3 n, float side) {
-  float ao = 1.0, w = ao_strength/ao_eps;
-  float dist = 2.0 * ao_eps;
+float ambient_occlusion(vec3 p, vec3 n, float DistAtp, float side) {
+  float ao_ed=DistAtp*100.0;
+  float ao = 1.0, w = ao_strength/ao_ed;//ps;
+  float dist = 2.0 * ao_ed;//ps;
 
   for (int i=0; i<5; i++) {
     float D = side * d(p + n*dist);
     ao -= (dist-D) * w;
     w *= 0.5;
-    dist = dist*2.0 - ao_eps;  // 2,3,5,9,17
+    dist = dist*2.0 - ao_ed;//ps;  // 2,3,5,9,17
   }
   return clamp(ao, 0.0, 1.0);
 }
 
-
 void main() {
+  // Interlaced stereoscopic eye fiddling
   vec3 eye_in = eye;
   eye_in += 2.0 * (fract(gl_FragCoord.y * 0.5) - .5) * speed * vec3(gl_ModelViewMatrix[0]);
 
-  vec3 p = eye_in, dp = normalize(dir);
+  vec3 p = eye_in, dp = normalize(dir);  
 
-  float totalD = 0.0, D = 3.4e38, extraD = 0.0, lastD;
+  float totalD = 0.0, D = 1000.0, extraD = 0.0, lastD;
+  D = d(p + totalD * dp);
+  float side = sign(D);
+  D = D*side; D *= 0.95;
+  totalD = D;
 
-  // Intersect the view ray with the fratal using raymarching.
+  // Intersect the view ray with the Mandelbox using raymarching.
   int steps;
-  for (steps=0; steps<max_steps; steps++) {
-    lastD = D;
-    D = d(p + totalD * dp);
-    if (extraD > 0.0 && D < extraD) {
-        totalD -= extraD;
-        extraD = 0.0;
-        D = 3.4e38;
-        steps--;
-        continue;
-    }
-    if (D < min_dist) break;
-    if (D > MAX_DIST) break;
-
+  for (steps=0; steps<max_steps && D > max(DE_EPS*totalD,0.0000152587890625) && totalD < MAX_DIST; steps++) {
+    D = side * d(p + totalD * dp);
+    D = D - min_dist * (totalD + D) ;
     totalD += D;
-    totalD += extraD = 0.096 * D * (D+extraD)/lastD;
   }
 
   p += totalD * dp;
-
+  //normal_eps = max(normal_eps,1.0*D);
   // Color the surface with Blinn-Phong shading, ambient occlusion and glow.
   vec3 col = backgroundColor;
 
   // We've got a hit or we're not sure.
   if (D < MAX_DIST) {
-    vec3 n = normal(p, D);
+    D = max(D, max(DE_EPS*totalD,0.0000152587890625));
+    vec3 n = side * normal(p, D);
     col = color(p);
-    col = blinn_phong(n, -dp, normalize(eye_in+vec3(lightVector.x,lightVector.y,-lightVector.z)+dp), col);
-    col = mix(aoColor, col, ambient_occlusion(p, n, .5));
+    col = blinn_phong(n, -dp, normalize(eye_in+lightVector+dp), col);
+    col = mix(aoColor, col, ambient_occlusion(p, n, D, side));
 
     // We've gone through all steps, but we haven't hit anything.
     // Mix in the background color.
@@ -165,7 +158,7 @@ void main() {
   // Glow is based on the number of steps.
   col = mix(col, glowColor, float(steps)/float(max_steps) * glow_strength);
 
-  // Write Z-buffer
+  // Write zBuffer
   float zFar = 5.0;
   float zNear = 0.0001;
   float a = zFar / (zFar - zNear);
