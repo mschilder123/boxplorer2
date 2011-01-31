6a40bd53c7fb5afc1c101f7f8adac26f67bc024b
marius.schilder@gmail.com <marius.schilder@gmail.com@29ca6bc3-2d16-8ea8-1f4c-e0f4d2eeda2c>
2011-01-31 23:46:27 +0000

    Cleaned up shaders to use same interlaced output and depth logic. All write depth info in alpha channel now.

--- trunk/cfgs/bulb.cfg.data/fragment.glsl
+++ trunk/cfgs/bulb.cfg.data/fragment.glsl
@@ -194,7 +194,8 @@ float ambient_occlusion(vec3 p, vec3 n) {
 
 void main() {
   vec3 eye_in = eye;
-  eye_in += 2.0 * (fract(gl_FragCoord.y * 0.5) - .5) * speed * vec3(gl_ModelViewMatrix[0]);
+  eye_in += 2.0 * (fract(gl_FragCoord.y * 0.5) - .5) * speed *
+      vec3(gl_ModelViewMatrix[0]);
 
   vec3 p = eye_in, dp = normalize(dir);
 
@@ -243,7 +244,7 @@ void main() {
   float zNear = 0.0001;
   float a = zFar / (zFar - zNear);
   float b = zFar * zNear / (zNear - zFar);
-  gl_FragDepth = (a + b / clamp(totalD/length(dir), 0., zFar));
-
-  gl_FragColor = vec4(col, 1);
+  float depth = (a + b / clamp(totalD/length(dir), zNear, zFar));
+  gl_FragDepth = depth;
+  gl_FragColor = vec4(col, depth);
 }
