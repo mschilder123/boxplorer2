85858bdbc83926430a8870ff5c928cdb0a0174fa
marius.schilder@gmail.com <marius.schilder@gmail.com@29ca6bc3-2d16-8ea8-1f4c-e0f4d2eeda2c>
2011-03-08 07:36:34 +0000

    Mucking with MengerSponge. Commented out though.

--- trunk/cfgs/menger-sphere.cfg.data/fragment.glsl
+++ trunk/cfgs/menger-sphere.cfg.data/fragment.glsl
@@ -24,29 +24,43 @@ uniform int max_steps;  // Maximum raymarching steps. {min=1 max=200}
 // Colors. Can be negative or >1 for interesting effects.
 #define backgroundColor par[7]
 #define surfaceColor1 par[2]
-//#define surfaceColor2 par[3]
-//#define surfaceColor3 par[4]
+#define surfaceColor2 par[3]
+#define surfaceColor3 par[4]
 #define specularColor par[5]
 #define glowColor par[6]
 #define lightVector par[1]
+#define rotationVector par[9]
 
-#define MOD par[8].x  // {min=1 max=5 step=.1}
-#define SCALE par[8].y  // {min=1 max=5 step=.1}
+#define MOD par[8].x  // {min=1 max=5 step=.01}
+#define SCALE par[8].y  // {min=1 max=5 step=.01}
 #define INTRA_D par[8].z  // {min=.01 max=10.0 step=.01}
-#define INITIAL_K par[0].x  // {min=0 max=5 step=.1}
+#define INITIAL_K par[0].x  // {min=0 max=5 step=.01}
 #define DE_EPS par[0].y  // {min=.0000001 max=.001 step=.0000001}
+
+#define Angle par[0].z  // {min=-3 max=3 step=.001}
+float csat = cos(Angle);
+float ssat = sin(Angle);
+float usat = 1.0-cos(Angle);
+vec3 z0 = normalize(vec3(par[9].x, par[9].y, par[9].z));
+mat3 RotationMatrix = mat3( z0.x*z0.x*usat + csat,      z0.x*z0.y*usat + z0.z*ssat, z0.x*z0.z*usat - z0.y*ssat,
+                            z0.y*z0.x*usat - z0.z*ssat, z0.y*z0.y*usat + csat,      z0.y*z0.z*usat + z0.x*ssat,
+			    z0.z*z0.x*usat + z0.y*ssat, z0.z*z0.y*usat - z0.x*ssat, z0.z*z0.z*usat + csat
+		      );
 
 vec3 aoColor = vec3(0, 0, 0);
 
-//Fuctions to call.
+//Functions to call.
 #define d d_SphereSponge
 #define color color_SphereSponge
+
+#if 1
 
 float d_SphereSponge(vec3 pos) {
   float k = INITIAL_K;
   float d = -100.0;
   for(int i=0; i<iters; i++) {
     vec3 x = mod(pos * k, MOD) - .5 * MOD;
+//    x *= RotationMatrix;
     float r = length(x);
     float d1 = (INTRA_D - r) / k;
     d = max(d, d1);
@@ -55,8 +69,59 @@ float d_SphereSponge(vec3 pos) {
   return d;
 }
 
+#else
+
+// from http://fractal.io/
+#define scale par[8].y  // {min=-10 max=10 step=.01}
+#define offset par[8].x  // {min=-3 max=3 step=.01}
+float d_SphereSponge(vec3 w) {
+    w = (w * 0.5 + vec3(0.5)) * scale;  // scale [-1, 1] range to [0, 1]
+    vec3 halfSpongeScale = vec3(.5) * scale;
+    vec3 v = abs(w - halfSpongeScale) - halfSpongeScale;
+    float d1 = max(v.x, max(v.y, v.z));     // distance to the box
+    float d = d1;
+    float p = 1.0;
+    float md = 10000.0;
+    vec3 cd = v;
+    
+    for (int i = 0; i < iters; i++) {
+        vec3 a = mod(3.0 * w * p, 3.0);
+        p *= 3.0;
+        
+        v = vec3(0.5) - abs(a - vec3(1.5)) + offset;
+        v *= RotationMatrix;
+
+        // distance inside the 3 axis aligned square tubes
+        d1 = min(max(v.x, v.z), min(max(v.x, v.y), max(v.y, v.z))) / p;
+        
+        // intersection
+        d = max(d, d1);
+      
+    }
+    
+    return d * 2.0 / scale;
+}
+
+#endif
+
 vec3 color_SphereSponge(vec3 pos) {
+#if 0
+  float k = INITIAL_K;
+  vec3 x;
+  float trap = 1.0;
+  for(int i = 0; i < color_iters; i++) {
+    x = mod(pos * k, MOD) - .5 * MOD;
+    float r = length(x);
+    float d1 = (INTRA_D - r) / k;
+    trap = min(trap, r);
+    k *= SCALE;
+  }
+  return mix(surfaceColor1,
+			 mix(surfaceColor2, surfaceColor3, length(x)),
+			 trap);
+#else
   return surfaceColor1;
+#endif
 }
 
 float normal_eps = 0.00001;
