68618ffd00cf2a247c891676bffd156dcc522004
marius.schilder <marius.schilder@gmail.com>
2011-06-19 23:30:05 +0000

    Tighter frame name matching.

--- utils/encode.bat
+++ utils/encode.bat
@@ -1,1 +1,1 @@
-mencoder -nosound -ovc lavc -lavcopts vcodec=mjpeg -o boxplorer-30fps.avi -mf type=tga:fps=30 mf://*.tga 
+mencoder -nosound -ovc lavc -lavcopts vcodec=mjpeg -o boxplorer-30fps.avi -mf type=tga:fps=30 mf://frame-*.tga 
