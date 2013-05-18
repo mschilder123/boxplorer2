fc8b6759d77b66c37224f2c2dee7e69aa628a6fd
marius.schilder@gmail.com <marius.schilder@gmail.com@29ca6bc3-2d16-8ea8-1f4c-e0f4d2eeda2c>
2013-05-18 22:32:14 +0000

    Add oculus support and rotations.

--- trunk/cfgs/rrrola.cfg.data/fragment.glsl
+++ trunk/cfgs/rrrola.cfg.data/fragment.glsl
@@ -32,7 +32,7 @@
 // Camera position and direction.
 varying vec3 eye, dir;
 varying float zoom;
-uniform float xres;
+uniform float xres, yres;
 
 // Interactive parameters.
 uniform vec3 par[10];
@@ -59,18 +59,46 @@ vec3 backgroundColor = vec3(0.07, 0.06, 0.16),
   glowColor = vec3(0.03, 0.4, 0.4),
   aoColor = vec3(0, 0, 0);
 
+float minRad2;
+vec4 scale;
+float absScalePowIters;
+mat3 rotationMatrix;
+
+void init() {
+#define rotationVector par[3]
+#define rotationAngle par[4].x  // { min=-5 max=5 step=.01}
+
+  // compute couple of constants.
+  minRad2 = clamp(MB_MINRAD2, 1.0e-9, 1.0);
+  scale = dvec4(MB_SCALE, MB_SCALE, MB_SCALE, abs(MB_SCALE)) / minRad2;
+  
+  float s = abs(MB_SCALE), ds = 1.0 / abs(MB_SCALE);
+  for (int i=0; i<iters; i++) s*= ds;
+  absScalePowIters = s;
+  
+  float csat = cos(rotationAngle);
+  float ssat = sin(rotationAngle);
+  float usat = 1.0 - csat;
+  vec3 u = normalize(rotationVector);
+  rotationMatrix = mat3(
+    u.x*u.x*usat + csat,     u.x*u.y*usat - u.z*ssat, u.x*u.z*usat + u.y*ssat,
+    u.y*u.x*usat + u.z*ssat, u.y*u.y*usat + csat,     u.y*u.z*usat - u.x*ssat,
+    u.z*u.x*usat - u.y*ssat, u.z*u.y*usat + u.x*ssat, u.z*u.z*usat + csat
+    );
+}
+
 float de_mandelbox(vec3 pos) {
-  float minRad2 = clamp(MB_MINRAD2, 1.0e-9, 1.0);
-  vec4 scale = vec4(MB_SCALE, MB_SCALE, MB_SCALE, abs(MB_SCALE)) / minRad2;
   vec4 p = vec4(pos,1.0), p0 = p;  // p.w is the distance estimate
   for (int i=0; i<iters; i++) {
+    p = vec4(rotationMatrix * p.xyz, p.w);
     p = vec4(clamp(p.xyz, -1.0, 1.0) * 2.0 - p.xyz, p.w);
     float r2 = dot(p.xyz, p.xyz);
     p *= clamp(max(minRad2/r2, minRad2), 0.0, 1.0);
     p = p*scale + p0;
+	if (r2 > 100.0) break;
   }
   return ((length(p.xyz) - abs(MB_SCALE - 1.0)) / p.w
-           - pow(abs(MB_SCALE), float(1-iters))) * DIST_MULTIPLIER;
+            - absScalePowIters) * 0.95 * DIST_MULTIPLIER;
 }
 DECLARE_DE(de_mandelbox)
 
@@ -80,8 +108,8 @@ vec3 c_mandelbox(vec3 pos) {
   vec3 scale = vec3(MB_SCALE, MB_SCALE, MB_SCALE) / minRad2;
   vec3 p = pos, p0 = p;
   float trap = 1.0;
-
   for (int i=0; i<color_iters; i++) {
+    p = vec3(rotationMatrix * p.xyz);
     p = clamp(p, -1.0, 1.0) * 2.0 - p;
     float r2 = dot(p, p);
     p *= clamp(max(minRad2/r2, minRad2), 0.0, 1.0);
@@ -133,31 +161,86 @@ float ambient_occlusion(vec3 p, vec3 n, float DistAtp, float side, float m_dist)
   return clamp(ao, 0.0, 1.0);
 }
 
-// ytalinflusa's noise [0..1>
-float pnoise(vec2 pt){return mod(pt.x*(pt.x+0.15731)*0.7892+pt.y*(pt.y+0.13763)*0.8547,1.0); }
+float hash( float n ) {
+    return fract(sin(n)*5345.8621276);
+}
 
-uniform float focus;  // {min=-10 max=30 step=.1} Focal plane devation from 20x speed.
-void setup_stereo(inout vec3 eye_in, inout vec3 dp) {
+float noise( in vec2 x ) {
+    vec2 p = floor(x);
+    vec2 f = fract(x);
+
+    f = f*f*(3.0-2.0*f);
+
+    float n = p.x + p.y*61.0;
+
+    float res = mix(mix( hash(n+  0.0), hash(n+  1.0),f.x),
+                    mix( hash(n+ 57.0), hash(n+ 58.0),f.x),f.y);
+
+    return fract(res);
+}
+
+uniform float focus;  // {min=-10 max=30 step=.01} Focal plane devation from 30x speed.
+bool setup_stereo(INOUT(vec3,eye_in), INOUT(vec3,dp)) {
 #if !defined(ST_NONE)
+#if defined ST_OCULUS
+  float halfx = xres / 2.0;
+
+  vec2 q;
+  if (sign(speed) < 0.0) {
+    // left. 45 pixel shift towards center. Eyeballed.
+    q = (gl_FragCoord.xy - vec2(focus + 45.0, 0.0)) / vec2(halfx, yres);
+  } else {
+    // right. 45 pixel shift towards center.
+    q = (gl_FragCoord.xy - vec2(halfx - focus - 45.0, 0.0)) / vec2(halfx, yres);
+  }
+  vec2 p = -1.0 + 2.0 * q;
+
+  // Oculus barrel distort parameters.
+  vec3 oculus_warp = vec3(1.0, 0.22, 0.24);  // k0, k1, k2
+  vec2 oculus_scale = vec2(0.3, 0.35);  // x/y ratio eyeballed
+  float r2 = dot(p, p);  // Radius squared, from center.
+  p *= oculus_scale * dot(oculus_warp, vec3(1.0, r2, r2*r2));
+  if (dot(p, p) > 0.10) { 
+    //discard;  // Don't waste time on pixels we can't see.
+    return false;
+  }
+
+  // Shift eye position, abs(speed) is half inter-occular distance.
+  vec3 eye_d = vec3(gl_ModelViewMatrix * vec4(speed, 0.0, 0.0, 0.0));
+  eye_in = eye + eye_d;
+
+  // Note: no asymmetric frustum for Rift.
+  dp = normalize(vec3(gl_ModelViewMatrix * vec4(p, 0.35, 0.0)));  // z value determines fov. Eyeballed.
+#else
 #if defined(ST_INTERLACED)
-  vec3 eye_d = vec3(gl_ModelViewMatrix * vec4( 4.0 * (fract(gl_FragCoord.y * 0.5) - .5) * abs(speed), 0, 0, 0));
+  vec3 eye_d = vec3(gl_ModelViewMatrix * vec4( 2.0 * (fract(gl_FragCoord.y * 0.5) - .5) * abs(speed), 0, 0, 0));
 #else
   vec3 eye_d = vec3(gl_ModelViewMatrix * vec4(speed, 0, 0, 0));
 #endif
   eye_in = eye + eye_d;
-  dp = normalize(dir * (focus + 20.0) * abs(speed) - eye_d);
+  // Construct asymmetric frustum.
+  dp = normalize(dir * (focus + 30.0) * abs(speed) - eye_d);
+#endif // ST_OCULUS
 #else  // ST_NONE
   eye_in = eye;
   dp = normalize(dir);
 #endif
+  return true;
 }
 
 void main() {
-  vec3 eye_in, dp; setup_stereo(eye_in, dp);
+  vec3 eye_in, dp; 
+
+  if (!setup_stereo(eye_in, dp)) {
+    gl_FragColor = vec4(0.0);
+    gl_FragDepth = 0.0;
+	return;
+  }
+
+  init();
 
   float m_zoom = zoom * 0.5 / xres;
-
-  float noise = pnoise(gl_FragCoord.xy);
+  float noise = noise(gl_FragCoord.xy / vec2(xres, yres));
 
   vec3 p = eye_in;
   float D = d(p);
