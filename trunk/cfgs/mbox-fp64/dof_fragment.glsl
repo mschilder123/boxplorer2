7e0a722bc01c95bbe73fabac44aee8ec123c3e18
marius.schilder@gmail.com <marius.schilder@gmail.com@29ca6bc3-2d16-8ea8-1f4c-e0f4d2eeda2c>
2014-05-27 07:48:15 +0000

    Damn apple glsl compiler

--- trunk/cfgs/mbox-fp64/dof_fragment.glsl
+++ trunk/cfgs/mbox-fp64/dof_fragment.glsl
@@ -13,7 +13,7 @@ varying vec2 iTexCoord;  // from vertex shader
 vec2 texRez = vec2(1.0/xres, 1.0/yres);
 
 // box
-const float gb_offset[13] = {-11.5, -9.5, -7.5, -5.5, -3.5, -1.5, 0, 1.5, 3.5, 5.5, 7.5, 9.5, 11.5};
+//const float gb_offset[13] = float[](-11.5, -9.5, -7.5, -5.5, -3.5, -1.5, 0.0, 1.5, 3.5, 5.5, 7.5, 9.5, 11.5);
 
 #define SQRT2 0.70710678118  // sqrt(2) * .5
 
@@ -21,7 +21,7 @@ const float gb_offset[13] = {-11.5, -9.5, -7.5, -5.5, -3.5, -1.5, 0, 1.5, 3.5, 5
 #if 1  // gamma? What gamma
 #define GAMMA 4.2
 #define P(x) pow((x), vec4(GAMMA,GAMMA,GAMMA,1.0))
-#define S(x) pow((x), vec4(1/GAMMA,1/GAMMA,1/GAMMA,1.0))
+#define S(x) pow((x), vec4(1.0/GAMMA,1.0/GAMMA,1.0/GAMMA,1.0))
 #else
 #define P(x) (x)
 #define S(x) (x)
@@ -32,7 +32,7 @@ vec4 mixDimension(vec2 coord) {
   const float bleedingMult = 30.0;
 
   float totalWeight = 0.0;
-  vec4 color = vec4(0.0);
+  vec4 color = vec4(0.0, 0.0, 0.0, 0.0);
 
   vec4 centerPixel = P(texture2D(iTexture, coord));
   float centerDepth = texture2D(iDepth, coord).x;
@@ -41,19 +41,21 @@ vec4 mixDimension(vec2 coord) {
 
   if (iBlurLevel == 0) {
     // (1, 0) or (0, 1) : square
-    xory = vec2(iXorY, 1. - iXorY) * texRez * centerPixel.a;
+    xory = vec2(float(iXorY), 1. - float(iXorY)) * texRez * centerPixel.a;
   } else {
     // (s2, s2) or (s2, -s2) : square rotate 45 degrees
-    xory = vec2(SQRT2, 2.*(.5 - iXorY) * SQRT2) * texRez * centerPixel.a;
+    xory = vec2(SQRT2, 2.*(.5 - float(iXorY)) * SQRT2) * texRez * centerPixel.a;
   }
 
+  float gb_offset = -11.5;
   for (int i = 0; i < 13; ++i) {
-    vec2 sampleCoord = coord + xory * gb_offset[i];
+    vec2 sampleCoord = coord + xory * gb_offset;
+    gb_offset += 2.0;
     vec4 samplePixel = P(texture2D(iTexture, sampleCoord));
     float sampleDepth = texture2D(iDepth, sampleCoord).x;
 
     float weight = (sampleDepth < centerDepth) ?
-        samplePixel.a * bleedingMult : 1.0f;
+        samplePixel.a * bleedingMult : 1.0;
     weight = (centerPixel.a > samplePixel.a + bleedingBias) ?
         weight : 1.0;
     weight = clamp(weight, 0.0, 1.0);
