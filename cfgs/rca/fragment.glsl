62690f237383c3876b1aac8241023ac3cad73c55
marius.schilder <marius.schilder@gmail.com>
2014-06-22 05:45:54 +0000

    Add reverse option

--- cfgs/rca/fragment.glsl
+++ cfgs/rca/fragment.glsl
@@ -4,13 +4,16 @@ varying vec3 dir;
 
 uniform sampler2D iChannel0;
 uniform float xres, yres, time;
-uniform vec3 par[1];
+uniform vec3 par[2];
 uniform int use_bg_texture;
 uniform int frameno;
 
 #define decayR par[0].x  // {min=0 max=1 step=.0001}
 #define decayG par[0].y  // {min=0 max=1 step=.0001}
 #define decayB par[0].z  // {min=0 max=1 step=.0001}
+
+#define random par[1].x  // {min=0 max=1 step=1}
+#define direction par[1].y  // {min=-1 max=1 step=2}
 
 #define backbuffer iChannel0
 vec2 position;
@@ -27,7 +30,7 @@ void isAlive(float dx, float dy, inout int count, int factor) {
 
 vec3 color(vec2 z) {
   // Ring o'fire
-  if (use_bg_texture == 0) {
+  if (use_bg_texture == 0 && random != 0) {
     if (length(z)<0.1 && length(z)>0.08) {
       return (rand(time*z) < 0.5 ? vec3(1.0,0.0,0.0) : vec3(0.0));
     }
@@ -38,6 +41,7 @@ vec3 color(vec2 z) {
   int count = 0;
   vec2 phase = -1.0 + 2.0 * floor(mod(gl_FragCoord.xy, 2.0));  // 1 or -1
   phase *= -1.0 + 2.0 * floor(mod(float(frameno), 2.0));  // 1 or -1
+  phase *= direction;
 
   isAlive(0.0,0.0, count, 1);
   
@@ -47,7 +51,7 @@ vec3 color(vec2 z) {
   isAlive(0.0, phase.y, count, 4);
   
   // Rules
-  if (count == int(-phase.x * phase.y + 3.0)) {
+  if (count == int(-direction * phase.x * phase.y + 3.0)) {
     return vec3(1.0, 1.0, 1.0);  // rotate clockwise into new life
   } else if (count > 1) {
     return v1 * vec3(1.0, par[0].y, par[0].z);  // stable
