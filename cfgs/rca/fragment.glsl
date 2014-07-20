277002af2f0a9be799c5ca5f9f41cc6440b682f5
marius.schilder <marius.schilder@gmail.com>
2014-07-20 21:14:42 +0000

    Frequency based coloring

--- cfgs/rca/fragment.glsl
+++ cfgs/rca/fragment.glsl
@@ -8,9 +8,7 @@ uniform vec3 par[2];
 uniform int use_bg_texture;
 uniform int frameno;
 
-#define decayR par[0].x  // {min=0 max=1 step=.0001}
-#define decayG par[0].y  // {min=0 max=1 step=.0001}
-#define decayB par[0].z  // {min=0 max=1 step=.0001}
+#define decay par[0].x  // {min=0 max=1 step=.0001}
 
 #define random par[1].x  // {min=0 max=1 step=1}
 #define direction par[1].y  // {min=-1 max=1 step=2}
@@ -24,9 +22,10 @@ float rand(vec2 co){
 }
 
 void isAlive(float dx, float dy, inout int count, int factor) {
-  float cur = texture2D(backbuffer, position + pixelSize*vec2( dx, dy )).r;
-  // Aliveness is tracked in r channel at 1.0
-  count += int(cur) * factor;
+  vec3 cur = texture2D(backbuffer, position + pixelSize*vec2( dx, dy )).rgb;
+  float alive =  max(cur.r, max(cur.g, cur.b));
+  // Aliveness is tracked in any channel at 1.0
+  count += int(alive) * factor;
 }
 
 vec3 color(vec2 z) {
@@ -49,7 +48,8 @@ vec3 color(vec2 z) {
   // Direction toggle.
   phase *= direction;
 
-  int count = int(cur.r);  // self 1 or 0.
+  float curMax = max(cur.r, max(cur.g, cur.b));
+  int count = int(curMax);
 
   // Count neighbours in relative 2x2.
   isAlive(phase.x, 0.0, count, 2);
@@ -87,6 +87,23 @@ int rule = 0xaaa8;
 int rotf = 0x0004;  // direction dependent toggle bits.
 int rotb = 0x0010;
 
+  // Scrambler
+  //                  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
+//const int rule[] = {0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1};
+  //                  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+//int rule = 0xb315;
+//int rotf = 0x0000;  // direction dependent toggle bits.
+//int rotb = 0x0000;
+
+#if 0
+  if (fract(float(frameno) / 2.0) >= .5) {
+    // Odd phase does Bounce gas.
+    rule = 0xe968;
+    rotf = 0;
+    rotb = 0;
+  }
+#endif
+
   // add rotf if going forward.
   rule += rotf * int(clamp(direction * phase.x * phase.y, 0.0, 1.0));
   // add rotb if going backward.
@@ -94,13 +111,23 @@ int rotb = 0x0010;
 
   if (fract(float(rule) / pow(2.0, float(count + 1))) < .5) {
     // dead: decay to black
-    return cur * par[0];
+    return cur * decay;
   } else if (fract(float(count) / 2.0) < .5) {
-    // born: start as full white
-    return vec3(1.0, 1.0, 1.0);
+    // born: color depending on state of decay.
+    float t = decay * decay;
+    if (curMax >= t) {
+      // period 2: green
+      return vec3(0.0, 1.0, 0.0);
+    } else if (curMax >= t*t) {
+      // period 4: white
+      return vec3(1.0, 1.0, 1.0);
+    } else {
+      // others: blue
+      return vec3(0.0, 0.0, 1.0);
+    }
   } else {
     // stable: decay to full red
-    return cur * vec3(1.0, par[0].y, par[0].z);
+    return vec3(1.0, cur.g * decay, cur.b * decay);
   }
 }
 
