7d59d64ece2cacda7b16a9d8f901320bd5337ed5
marius schilder <marius.schilder@gmail.com>
2026-01-24 00:42:56 -0800

    More gliders.

--- TGA2rle.cc
+++ TGA2rle.cc
@@ -1,9 +1,50 @@
 #include "TGA.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+const int patterns[] = {
+    0x3030, 0x6060, 0xc0c0, 0x0303, 0x0606, 0x0c0c,
+
+    0x0099, 0x0990, 0x9900,
+
+    0x0055, 0x0550, 0x5500,
+
+    0x0306, 0x0603, 0x3060, 0x6030,
+
+    0x060c, 0x0c06, 0x60c0, 0xc060,
+
+    0x2980,
+
+    0x0154, 0x1540, 0x02a8, 0x2a80,
+};
 
 int main(int argc, char *argv[]) {
   TGA tga;
   tga.readFile(argv[1]);
+
+  for (int y = 0; y < tga.height(); y += 4) {
+    for (int x = 0; x < tga.width(); x += 4) {
+      bool set = false;
+      for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+          set = set || tga.get(x + i, y + j);
+          tga.set(x + i, y + j, 0, 0, 0);
+        }
+      }
+      if (set) {
+        // pick random 4x4 walker pattern
+        int pattern =
+            patterns[rand() % (sizeof(patterns) / sizeof(patterns[0]))];
+        for (int i = 0; i < 4; ++i) {
+          for (int j = 0; j < 4; ++j) {
+            if (pattern & (1 << (i + 4 * j))) {
+              tga.set(x + i, y + j, 255, 255, 255);
+            }
+          }
+        }
+      }
+    }
+  }
 
   printf("x = %d, y = %d, rule = SingleRotation\n", tga.width(), tga.height());
 
