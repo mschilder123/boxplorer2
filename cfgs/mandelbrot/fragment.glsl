892e7026cc2112700d7c827750d796849cbc52c1
marius schilder <marius.schilder@gmail.com>
2026-02-15 00:28:01 -0800

    Tweak coloring.

--- cfgs/mandelbrot/fragment.glsl
+++ cfgs/mandelbrot/fragment.glsl
@@ -21,12 +21,6 @@ uniform int iters;  // Number of fractal iterations. {min=10 max=10000}
 uniform double dspeed;
 uniform float time;
 
-#define surfaceColor par[2]
-#define surfaceColor2 par[3]
-#define R par[2].x
-#define G par[2].y
-#define B par[2].z
-
 double fabs(double f) {
   return sign(f)*f;
 }
@@ -35,38 +29,6 @@ dvec2 complexMul(dvec2 a, dvec2 b) {
   return dvec2(a.x*b.x - a.y*b.y, a.x*b.y + a.y*b.x);
 }
 
-#if 0
-#define Divider par[0].x  // {min=0 max=50}
-#define Power par[0].y  // {min=0 max=6 step=1e-4}
-#define Radius par[0].z // {min=0 max=5 step=.01}
-
-// Mandelbrot for c.x,c.y
-vec3 getColor2D(dvec2 c, int maxIters) {
-  dvec2 z = vec2(0.0,0.0);
-  int i = 0;
-  double dist = 10000.0;
-  for (i = 0; i < maxIters; i++) {
-    z = complexMul(z,z) + c;
-    dist = min(dist, fabs(length(z)-Radius));
-    if (dot(z,z)> 4.0) break;
-  }
-  if (i < maxIters) {
-    // The color scheme here is based on one
-    // from Inigo Quilez's Shader Toy:
-    float co = float(i) + 1.0 - log2(.5*log2(float(dot(z,z))));
-    co = sqrt(co/256.0);
-    float co2 = dist * Divider;
-    float fac = clamp(1.0/pow(co2,Power),0.0,1.0);
-    return fac*vec3( .5+.5*cos(6.2831*co+R),
-      .5+.5*cos(6.2831*co+G),
-      .5+.5*cos(6.2831*co+B) );
-  } else {
-    return vec3(0.0);
-  }
-}
-#endif
-
-#if 1
 // MaddHattPatt youtube inspired
 
 vec3 HSVtoRGB(vec3 hsv) {
@@ -75,35 +37,35 @@ vec3 HSVtoRGB(vec3 hsv) {
   return hsv.z * mix(k.xxx, clamp(p - k.xxx, 0.0, 1.0), hsv.y);
 }
 
-vec3 SimpleSmoothedLoopedHue(int iteration, double dist, double z, int maxIters) {
-  float smoothIter = iteration + 1.0 - log2(log2(float(length(z))));
+vec3 SimpleSmoothedLoopedHue(int iteration, double dist, dvec2 z, int maxIters) {
+  float smoothIter = iteration + 1.0 - log2(log2(float(dot(z,z))));
   float t = float(smoothIter) / float(maxIters);
 
   vec3 color = vec3(0.0);
 
   if (iteration < maxIters) {
-    vec3 hsv = vec3(t * 3.0 - (0.2 * time), 1.0 * dist, 1.0);
+    vec3 hsv = vec3(t * 1.0 - (0.1 * time), 10.0 * dist, 1.0);
     color = HSVtoRGB(hsv);
   }
+
   return color;
 }
 
 #define TrapVector par[4]
 
 vec3 getColor2D(dvec2 c, int maxIters) {
-  dvec2 z = dvec2(0.0,0.0);
+  dvec2 z = dvec2(0.0);
   int i = 0;
-  double dist = 10000.0;
+  double dist = 1000.0;
   for (i = 0; i < maxIters; i++) {
     z = complexMul(z,z) + c;
-    dist = min(dist, length(z - TrapVector.xy));
-    if (dot(z,z) > 16.0) break;
+    dvec2 delta = z - TrapVector.xy;
+    dist = min(dist, dot(delta, delta));
+    double d = dot(z,z);
+    if (d > 4.0) break;
   }
   return SimpleSmoothedLoopedHue(i, dist, z, maxIters);
 }
-
-#endif
-
 
 void main() {
   dvec3 p = deye;
@@ -116,7 +78,7 @@ void main() {
   int maxIters = int(2.0*pow(log(float(3840.0 / totalD)), 1.8));
 
   vec3 col = vec3(0.0);
-  if (totalD > 0.0) col = getColor2D(dvec2(p), maxIters);
+  if (totalD > 0.0) col = getColor2D(dvec2(p), maxIters + iters);
 
   // Write zBuffer and pixel
   float zFar = 5.0;
